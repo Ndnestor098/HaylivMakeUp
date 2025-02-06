f5de64437cb2e57e0c167a72c0c457d623f1e7f7
ndnestor <trabajo.nestor.098@gmail.com>
2025-02-06 15:47:07 +0100

    Starting to create Footer

---
+++ resources/js/Layouts/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+export default function Footer() {
+    return(
+        <footer>
+            <section className="footer">
+                <div className="footer_logo_info">
+                    <div>
+                        <h1>Hayliv Makeup</h1>
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+
+                <div className="footer_social">
+
+                </div>
+
+                <div className="footer_form">
+
+                </div>
+            </section>
+
+            <section className="copyrigth">
+                
+            </section>
+        </footer>
+    )
+}
